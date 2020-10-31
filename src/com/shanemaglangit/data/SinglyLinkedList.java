144b22a567ffb3346daf71b48691317d9762a322
ShaneMaglangit <shanedmaglangit@gmail.com>
2020-10-31 14:50:09 +0800

    Added remove method

--- src/com/shanemaglangit/data/SinglyLinkedList.java
+++ src/com/shanemaglangit/data/SinglyLinkedList.java
@@ -30,18 +30,57 @@ public class SinglyLinkedList<T extends Comparable<T>> {
     public T get(int pos) {
         return getNode(head, pos).getValue();
     }
+    
+    public void remove(int pos) {
+        if(pos == 0) removeHead();
+        else remove(head, pos);
+    }
+
+    public void remove(T value) { remove(head, value); }
+
+    public void removeAll() { head = null; }
+
+    public boolean contains(T value) {
+        return find(head, value);
+    }
+
+    public void sort(boolean isAscending) {
+        quickSort(0, getSize() - 1, isAscending);
+    }
+
+    public int getSize() {
+        Node<T> currentNode = head;
+        int size = 0;
+
+        while(currentNode != null) {
+            currentNode = currentNode.getNext();
+            size++;
+        }
+
+        return size;
+    }
+
+    private void removeHead() {
+        if(head != null) head = head.getNext();
+    }
+
+    private void remove(Node<T> node, int pos) {
+        if(node == null || node.getNext() == null) throw new IndexOutOfBoundsException();
+        else if(pos == 1) node.setNext(node.getNext().getNext());
+        else remove(node.getNext(), pos - 1);
+    }
+
+    private void remove(Node<T> node, T value) {
+        if(node == null || node.getNext() == null) throw new IndexOutOfBoundsException();
+        else if(node.getNext().getValue().equals(value)) node.setNext(node.getNext().getNext());
+        else remove(node.getNext(), value);
+    }
 
     private Node<T> getNode(Node<T> node, int pos) {
         if(node == null) throw new IndexOutOfBoundsException();
         else if(pos == 0) return node;
         else return getNode(node.getNext(), pos - 1);
     }
-    
-    public void remove(int pos) { }
-
-    public void remove(Node<T> node) { }
-
-    public void removeAll() { head = null; }
 
     private void insertAtPos(Node<T> node, T value, int pos) {
         if(node.getNext() != null) insertAtPos(node.getNext(), value, pos - 1);
@@ -62,30 +101,10 @@ public class SinglyLinkedList<T extends Comparable<T>> {
         head = node;
     }
 
-    public int getSize() {
-        Node<T> currentNode = head;
-        int size = 0;
-
-        while(currentNode != null) {
-            currentNode = currentNode.getNext();
-            size++;
-        }
-
-        return size;
-    }
-
-    public boolean contains(T value) {
-        return find(head, value);
-    }
-
     private boolean find(Node<T> node, T value) {
         if(node == null) return false;
         else if (node.getValue().equals(value)) return true;
         else return find(node.getNext(), value);
-    }
-
-    public void sort(boolean isAscending) {
-        quickSort(0, getSize() - 1, isAscending);
     }
 
     private void quickSort(int low, int high, boolean isAscending) {
